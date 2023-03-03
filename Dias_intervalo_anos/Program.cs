509486216e888cbcb3ffd2d4ddb5d725934ee107
maikeliani <maikel.iani31@gmail.com>
2023-03-03 14:48:48 -0300

    Add project files.

---
+++ Dias_intervalo_anos/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int first_year, last_year, days = 0, count = 0;
+
+        Console.WriteLine(" Informe o primeiro ano " );
+        first_year = int.Parse(Console.ReadLine());
+        last_year = first_year - 1;
+
+        while (first_year > last_year)
+        {
+            Console.WriteLine( "Informe o segundo ano " );
+            last_year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" O segundo ano deve ser maior que o primeiro ano!!!");
+
+        }
+
+        days = (last_year - first_year + 1) * 365;
+        count = first_year;
+
+        do
+        {
+           
+            if ( (count % 4 == 0) && (count % 100 != 0) || (count % 400 == 0) )
+            {
+                days += 1;
+            }
+            count++;
+        } while (count<= last_year );
+        Console.WriteLine(  days);
+
+    }
+}
